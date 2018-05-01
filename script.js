905aac1265028a9cbbc301dfccac1238bfbf5324
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-05-01 17:08:59 +0300

    format

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let input = document.querySelector('.input__field');
-input.oninput = function(){
-let output=document.querySelector('.output__field');
-let tag=/</g;
-let starred=/\*(.+?)\*/ig;
-let sharped=/#(.+?)\n/ig;
-    output.innerHTML=input.value.replace(tag, '&lt;');
-    output.innerHTML=output.innerHTML.replace(starred, '<b>$1</b>');
-    output.innerHTML=output.innerHTML.replace(sharped, '<h1>$1</h1>');
+let input = document.querySelector(".input__field");
+input.oninput = function() {
+  let output = document.querySelector(".output__field");
+  let tag = /</g;
+  let starred = /\*(.+?)\*/gi;
+  let sharped = /#(.+?)\n/gi;
+  output.innerHTML = input.value.replace(tag, "&lt;");
+  output.innerHTML = output.innerHTML.replace(starred, "<b>$1</b>");
+  output.innerHTML = output.innerHTML.replace(sharped, "<h1>$1</h1>");
 };
