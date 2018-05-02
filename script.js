cad1a4a18027cfcedcecde9858e40b9537f9ca0e
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-05-02 19:24:43 +0300

    const

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let input = document.querySelector(".input__field");
-let output = document.querySelector(".output__field");
-let tag = /</g;
-let starred = /\*(.+?)\*/gi;
-let sharped = /#(.+?)\n/gi;
+const input = document.querySelector(".input__field");
+const output = document.querySelector(".output__field");
+const tag = /</g;
+const starred = /\*(.+?)\*/gi;
+const sharped = /#(.+?)\n/gi;
 input.oninput = () => {
   output.innerHTML = input.value.replace(tag, "&lt;");
   output.innerHTML = output.innerHTML.replace(starred, "<b>$1</b>");
