8818fb9506353488519d33079b963cbc99531848
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-05-02 14:16:01 +0300

    formatted

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let input = document.querySelector(".input__field");
-input.oninput = function() {
-  let output = document.querySelector(".output__field");
-  let tag = /</g;
-  let starred = /\*(.+?)\*/gi;
-  let sharped = /#(.+?)\n/gi;
+let output = document.querySelector(".output__field");
+let tag = /</g;
+let starred = /\*(.+?)\*/gi;
+let sharped = /#(.+?)\n/gi;
+input.oninput = () => {
   output.innerHTML = input.value.replace(tag, "&lt;");
   output.innerHTML = output.innerHTML.replace(starred, "<b>$1</b>");
   output.innerHTML = output.innerHTML.replace(sharped, "<h1>$1</h1>");
